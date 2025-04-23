e1eb3afff87dd92f4769bc25eb0e541b90efd4d9
Alexandre Roy <alexandre15roy@gmail.com>
2025-04-23 08:33:53 -0400

    Ajoutez des fichiers projet.

---
+++ Models/Constellation.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+namespace Planetarium.Models
+{
+    //TODO : Compléter la classe Constellation avec ces attributs, propriétés, constructeur et méthodes
+    public class Constellation
+    {
+
+
+    }
+}
---
+++ Models/Etoile.cs
@@ -0,0 +1,9 @@
+﻿namespace Planetarium.Models
+{
+    ////TODO : Compléter la classe Etoile avec ces attributs, propriétés, constructeur et méthodes
+    public class Etoile
+    {
+
+
+    }
+}
