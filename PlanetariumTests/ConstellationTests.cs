6c1c3174fc7aa7d1743fb173a0187d995af754a4
Alexandre Roy <alexandre15roy@gmail.com>
2025-04-25 13:35:09 -0400

    CHORE (tests) - Ajout des tests vides

---
+++ PlanetariumTests/ConstellationTests.cs
@@ -0,0 +1,48 @@
+using Planetarium.Models;
+using System.Reflection.PortableExecutable;
+using System.Text;
+namespace PlanetariumTests
+{
+    public class ConstellationTests
+    {
+        [Fact]
+        public void Ajout_Dune_Premiere_Etoile_Doit_Correspondre_A_La_Racine_De_La_Constellation()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [Fact]
+        public void Etoile_Qui_Possede_Magnitude_Inferieure_A_Racine_Doit_Etre_Ajoutee_A_Gauche()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [Fact]
+        public void Etoile_Qui_Possede_Magnitude_Superieure_A_Racine_Doit_Etre_Ajoutee_A_Droite()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [Fact] 
+        public void Verifier_Si_Etoile_Existe_Deja_Dans_Constellation()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+    }
+}
---
+++ PlanetariumTests/EtoileTests.cs
@@ -0,0 +1,18 @@
+using Planetarium.Models;
+using System.Reflection.PortableExecutable;
+using System.Text;
+namespace PlanetariumTests
+{
+    public class EtoileTests
+    {
+        [Fact]
+        public void Attribut_De_Classe_Ne_Doit_Pas_Etre_Null_String()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+    }
+}
